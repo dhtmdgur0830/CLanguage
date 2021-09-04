bc47f59bea0c766ee1dda1cab2b33afa199f5d7d
dhtmdgur <osh4890@gmail.com>
2021-09-04 23:51:03 +0900

    20210904

--- CLanguage/소스.c
+++ CLanguage/소스.c
@@ -5,6 +5,11 @@ void 부호바꾸기();
 void 증감연산자();
 void sizeOf연산자();
 void Pinter();
+void if문실수();
+void if문실수2();
+void IF문_Integer인식();
+void 삼항연산자와_ifelse();
+void switch문();
 
 void main()
 {
@@ -75,4 +80,81 @@ void Pinter()
 
 	*pnData = 20;
 	printf("%d\n", nData);
+}
+
+
+
+void if문실수()
+{
+	int nData = 18;
+	if (nData - 11) // 0만 거짓으로 인식한다. 즉, -1은 참으로 인식한다.
+	{
+		printf("Success");
+	}
+}
+
+void if문실수2()
+{
+	int nData = 10;
+
+
+	if (nData == 20); // 세미콜론이 있으면 if문이 끝났다고 인식한다.
+	{
+		puts("Same");
+	}
+}
+
+void IF문_Integer인식()
+{
+	if (1) // 1은 true
+	{
+		printf("실행이 됨");
+	}
+
+	if (0) // 0은 False
+	{
+		printf("실행되지 않음");
+	}
+}
+
+void 삼항연산자와_ifelse()
+{
+	int data = 0, x = 10, y = 20;
+
+	data = x < y ? x : y;
+	printf("%d\n", data);
+
+	if (x < y)
+	{
+		data = x;
+	}
+	else
+	{
+		data = y;
+	}
+
+	printf("%d\n", data);
+}
+
+void switch문()
+{
+	int input = 0;
+	char chCredit = 'X';
+	scanf_s("%d", &input);
+	switch (input / 10)
+	{
+	case 10:
+	case 9:
+		chCredit = 'A';
+		break;
+	case 8:
+		chCredit = 'B';
+		break;
+	case 7:
+		chCredit = 'C';
+		break;
+	default:
+		chCredit = 'F';
+	}
+	printf("Credit : %c\n", chCredit);
 }
